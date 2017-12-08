7b7e9afda78c69c93dd9ae7425f31fe6f7155ac7
deathsy <kittipong.sri@mail.kmutt.ac.th>
2017-12-08 20:18:23 +0700

    edit middleware

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const line = require('@line/bot-sdk');
+const express = require('express')
+const bodyParser = require('body-parser')
+const line = require('@line/bot-sdk')
 
 const config = {
   channelAccessToken: '3Rs1Noy94KmJ0dM9zs+3mksgZjDl6jCIjpwRlQ2WtwL9I3snNzNy0l2mq+EIJ8TtMczoocQNlmIA55ZgaRwkJX5AuctxrsnbDcbUenivetzgOr5frzACT7y9bIZXzcZdm60cdjfyjN4sKhvx6WeS5wdB04t89/1O/w1cDnyilFU=',
@@ -8,7 +8,6 @@ const config = {
 }
 
 const app = express()
-app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.json('hello')
@@ -16,17 +15,16 @@ app.get('/', (req, res) => {
 
 app.post('/webhook', line.middleware(config), (req, res) => {
   console.log(req)
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then(result => res.json(result))
+  Promise.all(req.body.events.map(handleEvent)).then(result => res.json(result))
 })
 
 const client = new line.Client(config)
 
 const handleEvent = event => {
-  if (event.type !== 'message' || event.message.type !== 'text')
+  if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null)
-    
+  }
+
   return client.replyMessage(event.replyToken, {
     type: 'text',
     text: event.message.text
